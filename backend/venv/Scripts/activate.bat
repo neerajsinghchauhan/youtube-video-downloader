bfbbff6acabb1220ce2c40eb199c956874697267
Neeraj <uhodigas@fryferno.com>
2024-05-18 19:34:52 -0400

    Second commit

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\trc\Downloads\Youtube\youtube-downloader\netlify\functions\flask\venv
+set VIRTUAL_ENV=C:\Users\trc\Downloads\Youtube\youtube-downloader\netlify\functions\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
