c3fb550f0f9fe5e3635fca1682b96bc9b9e78aab
Neeraj <uhodigas@fryferno.com>
2024-05-23 07:23:28 -0400

    port change

--- frontend/src/components/VideoDownloader.js
+++ frontend/src/components/VideoDownloader.js
@@ -19,7 +19,7 @@ const VideoDownloader = () => {
     setVideoTitle('');
 
     try {
-      const response = await axios.post('http://localhost:5000/download', { url, format }, {
+      const response = await axios.post('http://127.0.0.1:5000/download', { url, format }, {
         responseType: 'blob',
         onDownloadProgress: (progressEvent) => {
           const total = progressEvent.total;
